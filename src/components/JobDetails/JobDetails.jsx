af7be74f561e67fc6832d09114298ba25f12f2ed
MohammadShahansha <shahansha.cou.ict11@gmail.com>
2023-04-20 21:11:49 +0600

    added view detailes fully

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -17,20 +17,56 @@ const JobDetails = () => {
         }
     }, [id])
     console.log(jobs);
-    const { jobDescription, salary, requirements, responsibilities, experiences, jobTitle, phone, email, address } = jobs;
+    const { jobDescription, salary, requirements, responsibilities, experiences, jobTitle, phone, email, address, location } = jobs;
     return (
-        <div className='job-feature-container'>
-            <div className='job-feature-left-part'>
-                <p><span>Job Description:</span>{jobDescription}</p>
-                <p><span>Job Responsibility:</span>{responsibilities}</p>
-                <h5>Educational Requirements:</h5>
-                <p>{requirements}</p>
-                <h5>Experiences:</h5>
-                <p>{experiences}</p>
+        <div>
+            <div className='job-feature-container'>
+                <div className='job-feature-left-part'>
+                    <p><span>Job Description:</span>{jobDescription}</p>
+                    <p><span>Job Responsibility:</span>{responsibilities}</p>
+                    <h5>Educational Requirements:</h5>
+                    <p>{requirements}</p>
+                    <h5>Experiences:</h5>
+                    <p>{experiences}</p>
+                </div>
+                <div>
+                    <div className='fob-feature-right-part'>
+                        <h2>Job Details</h2>
+                        <hr />
+                        <div className='salary-detailees info'>
+                            <img src="../../../public/Icons/Frame.png" alt="" />
+                            <h5>Salary:</h5>
+                            <p>{salary}(per month)</p>
+                        </div>
+                        <div className='title-detailes info'>
+                            <img src="../../../public/Icons/Frame-1.png" alt="" />
+                            <h5>Job Title:</h5>
+                            <p>{jobTitle}</p>
+                        </div>
+                        <h5>Contact Information</h5>
+                        <hr />
+                        <div className='phone-detailes info'>
+                            <img src="../../../public/Icons/Frame-2.png" alt="" />
+                            <h5>Phone:</h5>
+                            <p>{phone}</p>
+                        </div>
+                        <div className='email-detailes info'>
+                            <img src="../../../public/Icons/Frame-3.png" alt="" />
+                            <h5>Email:</h5>
+                            <p>{email}</p>
+                        </div>
+                        <div className='address-detailes info'>
+                            <img src="../../../public/Icons/Frame-4.png" alt="" />
+                            <h5>Address:</h5>
+                            <p>{address}</p>
+                        </div>
+                        <p className='location'>{location}</p>
+                    </div>
+                    <button className='apply-button'>Apply Now</button>
+                </div>
+
             </div>
-            <div className='fob-feature-right-part'>
-                <h2>Job Details</h2>
-            </div>
+
         </div>
     );
 };
