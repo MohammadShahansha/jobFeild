0b08579314c4f94d14e317a4305b965b121cb4a2
MohammadShahansha <shahansha.cou.ict11@gmail.com>
2023-04-21 15:16:51 +0600

    added button with changes some information

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import './Cart.css';
 import { Link } from 'react-router-dom';
 
-const Cart = ({cart}) => {
+const Cart = ({ cart }) => {
     console.log(cart);
-    const {img,jobTitle,jobDuration ,jobQuality,company,location,salary,id}= cart;
+    const { img, jobTitle, jobDuration, jobQuality, company, location, salary, id } = cart;
     return (
-        <div  className="applied-container">
+        <div className="applied-container">
             <div className='applied-contaner-left-part'>
                 <img className="company-img" src={img} alt="" />
                 <div className='applied-info'>
                     <h4>{jobTitle}</h4>
                     <p>{company}</p>
-                    <button>{jobQuality}</button>
-                    <button>{jobDuration}</button>
+                    <div className='button'>
+                        <button>{jobQuality}</button>
+                        <button>{jobDuration}</button>
+                    </div>
                     <div className='location-salary'>
                         <p><img src="../../../public/Icons/Location Icon.png" alt="" />{location}</p>
                         <p><img src="../../../public/Icons/Frame.png" alt="" />${salary}</p>
--- src/components/Feature/Feature.jsx
+++ src/components/Feature/Feature.jsx
@@ -3,26 +3,26 @@ import './Feature.css';
 import JobDetails from '../JobDetails/JobDetails';
 import { Link } from 'react-router-dom';
 
-const Feature = ({feature}) => {
-    const {img,jobTitle,company,location,salary,id} = feature;
-   
+const Feature = ({ feature }) => {
+    const { img, jobTitle, jobDuration, jobQuality, company, location, salary, id } = feature;
+
     return (
         <div className='features'>
             <img src={img} alt="" />
             <h2>{jobTitle}</h2>
             <p className='company'>{company}</p>
             <div className='two-button'>
-                <button>Remote</button>
-                <button className='full-time'>Full Time</button>
+                <button>{jobQuality}</button>
+                <button className='full-time'>{jobDuration}</button>
             </div>
             <div className='locaton-salary'>
-            <img src="../../../public/Icons/Location Icon.png" alt="" />
+                <img src="../../../public/Icons/Location Icon.png" alt="" />
                 <p>{location}</p>
                 <p>Salary:${salary}</p>
             </div>
-           <p><Link className='view-details' to={`/jobdetaile/${id}`}>View Details</Link></p>
-            
-            
+            <p><Link className='view-details' to={`/jobdetaile/${id}`}>View Details</Link></p>
+
+
         </div>
     );
 };
